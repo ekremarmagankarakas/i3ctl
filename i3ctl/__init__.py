91a176222b29dfc050d092882846697b533f9661
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2025-05-08 20:47:18 +0300

    credentials change

--- i3ctl/__init__.py
+++ i3ctl/__init__.py
@@ -5,5 +5,5 @@ i3ctl - A command line utility for managing i3 window manager settings.
 # Single source of truth for version
 __version__ = "0.1.0"
 __author__ = "i3ctl Developers"
-__email__ = "info@i3ctl.org"
-__url__ = "https://github.com/i3ctl/i3ctl"
+__email__ = "ekremarmagankarakas@gmail.com"
+__url__ = "https://github.com/ekremarmagankarakas/i3ctl"
